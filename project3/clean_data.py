41af5664c34adbfdcdf2d36d2087406651070330
imgomez0127 <ianm0127@hotmail.com>
2020-05-14 17:16:51 -0400

    Added data cleaning scripty

--- project3/clean_data.py
+++ project3/clean_data.py
@@ -1,6 +1,65 @@
 import pandas as pd 
 
+acronyms = {
+    "Alabama":"AL",
+    "Alaska":"AK",
+    "Arizona":"AZ",
+    "Arkansas":"AR",
+    "California":"CA",
+    "Colorado":"CO", "Connecticut":"CT",
+    "Delaware":"DE",
+    "Florida":"FL",
+    "Georgia":"GA",
+    "Hawaii":"HI",
+    "Idaho":"ID",
+    "Illinois":"IL",
+    "Indiana":"IN",
+    "Kansas":"KS",
+    "Kentucky":"KY",
+    "Louisiana":"LA",
+    "Maine":"ME",
+    "Maryland":"MD",
+    "Massachusetts":"MA",
+    "Michigan":"MI",
+    "Minnesota":"MN",
+    "Mississippi":"MS",
+    "Missouri":"MO",
+    "Montana":"MT",
+    "Nebraska":"NE",
+    "Nevada":"NV",
+    "New Hampshire":"NH",
+    "New Jersey":"NJ",
+    "New Mexico":"NM",
+    "New York":"NY",
+    "North Carolina":"NC",
+    "North Dakota":"ND",
+    "Ohio":"OH",
+    "Oklahoma":"OK",
+    "Oregon":"OR",
+    "Pennsylvania":"PA",
+    "Rhode Island":"RI",
+    "South Carolina":"SC",
+    "South Dakota":"SD",
+    "Tennessee":"TN",
+    "Texas":"TX",
+    "Utah":"UT",
+    "Vermont":"VT",
+    "Virginia":"VA",
+    "Washington":"WA",
+    "West Virginia":"WV",
+    "Wisconsin":"WI",
+    "Wyoming":"WY",
+    "Iowa":"IA"
+}
 df = pd.read_csv("Unemployment_Data.csv")
 print(len(df))
 df2 = df.drop(df[df["State/Area"] == "Los Angeles County"].index)
-print(len(df2))
+df3 = df2.drop(df2[df2["State/Area"] == "New York city"].index)
+df4 = df3.drop(df3[df3["State/Area"] == "District of Columbia"].index)
+df5 = df4.drop(df4[df4["Monet"]] != 1)
+print(len(df5))
+states = df5["State/Area"].map(lambda x: acronyms[x])
+print(states)
+df4["State/Area"] = states
+print(df5)
+df5.to_csv("cleaned_median_income.csv")
